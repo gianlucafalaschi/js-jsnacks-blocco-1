80a83e8ce6bdc85c42cd15eee2a38356c80aa770
gianlucafalaschi <gian100gian@outlook.it>
2024-03-11 17:41:18 +0100

    feat: snack 4 average

--- snack-4/js/script.js
+++ snack-4/js/script.js
@@ -2,16 +2,24 @@
 
 //dichiara variabile sum con valore 0
 let sum = 0;
-
+let average
 //ciclo for per avere numeri da 1 a 10
 for(let i = 1; i <= 10; i++) {
     //console.log(i);
     let number = i; 
      //ad ogni numero sommo il precedente
      sum = sum + number;
-     console.log(sum);
+     //console.log(sum);
+
 }
 
+
+
+// Mostro la somma dei numeri
 alert(sum);
-    // divido la somma totale per il numero totale dei numeri 
+
+//calcolo la media dei numeri
+let averageNumber = sum / 10; 
+// Mostro la media dei numeri
+alert(averageNumber);
 
