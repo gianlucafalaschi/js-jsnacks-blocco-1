cb41d298525192c8729a37220ff877fe75e6995e
gianlucafalaschi <gian100gian@outlook.it>
2024-03-11 17:06:36 +0100

    feat: snack  3

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -1,3 +1,21 @@
 // - Crea un array vuoto.
 //  Chiedi per 6 volte all’utente di inserire un numero,
 //  se è dispari inseriscilo nell’array.
+
+// crea array vuoto
+const numberList = [];
+// chiedi per 6 volte all'utente di inserire un numero
+for(let i = 0; i < 6; i++) {
+    let userNumber = parseInt(prompt('Dimmi un numero'));
+    //console.log(userNumber);
+
+    //controlla se il numero e' pari o dispari
+    if(userNumber % 2 !== 0) {
+        //se il numero e' dispari lo inserisco nell'array
+        numberList.push(userNumber);
+
+    }
+}
+
+
+console.log(numberList);
