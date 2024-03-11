951a6ed7a2996c0935381b5fc7ec91e26b5beb75
gianlucafalaschi <gian100gian@outlook.it>
2024-03-11 16:24:46 +0100

    feat: snack2 sum

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -1,2 +1,18 @@
 // - Il software deve chiedere per 5 volte all’utente di inserire un numero.
 //  Il programma stampa la somma di tutti i numeri inseriti.
+
+//creo una variabile sum di valore 0
+
+let sum = 0;
+//chiedo all'utente 5 volte un numero
+for(let i = 0; i < 5; i++) {
+    let userNumber = parseInt(prompt('Dammi un numero'));
+    //console.log(userNumber);
+    
+    //lo sommo al numero precedente
+    sum = sum + userNumber;
+    console.log(sum);
+}
+
+
+//STAMPO LA SOMMA DI TUTTI I NUMERI
