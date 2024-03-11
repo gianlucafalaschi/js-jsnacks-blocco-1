b05155ef69beccefbfb03acbcd1af5c2e5d2d8f0
gianlucafalaschi <gian100gian@outlook.it>
2024-03-11 18:51:47 +0100

    feat: snack bonus Math.max function added

--- snack-bonus/js/script.js
+++ snack-bonus/js/script.js
@@ -1,1 +1,13 @@
 // - Dato un array contenente 10 numeri, stampare in console il numero più grande.
+
+// dichiaro un array con 10 numeri
+const numbersList = [1,2,3,4,5,6,7,8,9,10];
+// console.log(numbersList);
+// uso la funzione Math.max() per trovare il numero piu' grande 
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/max
+let biggestNumber = Math.max(...numbersList);
+//console.log(biggestNumber);
+// stampo il numero piu' grande
+let message = "il numero piu' grande è " + biggestNumber;
+
+alert(message);
