b4c1c0978d8d23b8589ddd91c32a1097598b657c
gianlucafalaschi <gian100gian@outlook.it>
2024-03-11 17:19:18 +0100

    feat: snack 4 sum

--- snack-4/js/script.js
+++ snack-4/js/script.js
@@ -1,1 +1,17 @@
 //- Calcola la somma e la media dei primi 10 numeri.
+
+//dichiara variabile sum con valore 0
+let sum = 0;
+
+//ciclo for per avere numeri da 1 a 10
+for(let i = 1; i <= 10; i++) {
+    //console.log(i);
+    let number = i; 
+     //ad ogni numero sommo il precedente
+     sum = sum + number;
+     console.log(sum);
+}
+
+   
+    // divido la somma totale per il numero totale dei numeri 
+
