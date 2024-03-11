b4357f0efc249ec9b92d3cbb9fbfbd22a2ee495c
gianlucafalaschi <gian100gian@outlook.it>
2024-03-11 16:05:54 +0100

    feat: snack 1

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,1 +1,23 @@
-// -Chiedere all'utente 5 numeri. Inserire i numeri nell'array ma senza creare duplicati (inserire il numero nell'array solo se non è già presente).
+// -Chiedere all'utente 5 numeri. Inserire i numeri nell'array ma senza creare duplicati 
+// (inserire il numero nell'array solo se non è già presente).
+
+
+
+//dichiaro un array vuoto
+const numberList = [];
+
+// chiedo all'utente 5 volte un numero
+for(let i = 0; i < 5; i++) {
+    const userNumber = parseInt(prompt('Dammi un numero'));
+    // console.log(userNumber); 
+
+    //controllo che il numero non sia nell'array
+    if(numberList.includes(userNumber) === false) {
+        // se il numero non e' nell'array lo aggiungo nell'array
+        numberList.push(userNumber);
+    }
+        
+
+}
+
+console.log(numberList);
